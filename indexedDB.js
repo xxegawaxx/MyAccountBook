3540b3957450a710925ef5a149c62c837b4e6eaf
xxegawaxx <e.masashi.st@gmail.com>
2021-08-07 12:30:52 +0900

    データベースとオブジェクトストアの作成

--- indexedDB.js
+++ indexedDB.js
@@ -1,1 +1,23 @@
-//第4章：収支のデータをデータベースに記録
+// indexedDBの設定
+const dbName = 'kakeiboDB';
+const storeName = 'kakeiboStore';
+const dbVersion = 1;
+
+// データベース接続
+let database = indexedDB.open(dbName, dbVersion);
+
+database.onupgradeneeded = function (event) {
+  let db = event.target.result;
+  console.log(db);
+  db.createObjectStore(storeName, { keyPath: 'id' });
+  console.log('データベースを新規作成しました');
+};
+
+database.onsuccess = function (event) {
+  let db = event.target.result;
+  db.close();
+  console.log('データベースに接続できました');
+};
+database.onerror = function (event) {
+  console.log('データベースに接続できませんでした');
+};
