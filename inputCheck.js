ce594d0169dc6475de7da7351ec9ef1afc018fb4
xxegawaxx <e.masashi.st@gmail.com>
2021-08-07 11:17:08 +0900

    入力チェック

--- inputCheck.js
+++ inputCheck.js
@@ -4,4 +4,37 @@ function disableSelectBox(disabled) {
 }
 
 /** 収支入力フォームの内容チェック */
-function inputCheck() {}
+function inputCheck() {
+  let result = true;
+
+  let radio = document.getElementsByName('balance');
+  let balance = null;
+
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked === true) {
+      balance = radio[i].value;
+      break;
+    }
+  }
+
+  let date = document.getElementById('date').value;
+  let category = document.getElementById('category').value;
+  let amount = document.getElementById('amount').value;
+  let memo = document.getElementById('memo').value;
+
+  // 入力チェック
+  if (date === '') {
+    result = false;
+    alert('日付が未入力です');
+  } else if (category === '-選択してください-' && balance === '支出') {
+    result = false;
+    alert('カテゴリを選択してください');
+  } else if (amount === 0 || amount === '') {
+    result = false;
+    alert('金額が未入力です');
+  } else if (memo === '') {
+    result = false;
+    alert('メモが未入力です');
+  }
+  return result;
+}
