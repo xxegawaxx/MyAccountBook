f6a2544e7cb1ebac0eb9b892c211c1e60f415d16
egawa <e.masashi.st@gmail.com>
2021-08-07 09:28:04 +0900

    ラジオボタンをクッリク時の動作

--- inputCheck.js
+++ inputCheck.js
@@ -1,1 +1,7 @@
-//第3章：UIの動作
+/** ラジオボタンによりカテゴリの有効無効切り替え */
+function disableSelectBox(disabled) {
+  document.getElementById('category').disabled = disabled;
+}
+
+/** 収支入力フォームの内容チェック */
+function inputCheck() {}
